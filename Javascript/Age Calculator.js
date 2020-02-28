a30db1bc43ff6eeba16fecbd9e7e0406440e1095
Jonathan Chang <jonoleighchang@gmail.com>
2020-02-27 20:11:36 -0400

    Minor Javascript code

---
+++ Javascript/Age Calculator.js
@@ -0,0 +1,5 @@
+var userAge = prompt("How old are you?");
+
+var ageInDays = userAge * 365.25;
+
+alert("Age " + userAge + " is roughly " + ageInDays + " days old");
---
+++ Javascript/JS Stalker Exercise.js
@@ -0,0 +1,8 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+var userFullName =  userFirstName + " "+ userLastName
+
+console.log("Your full name is " + userFullName);
+console.log("You are " + userAge + " years old");
