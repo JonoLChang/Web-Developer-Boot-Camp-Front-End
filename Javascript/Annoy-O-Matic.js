847fe7ac04d746839f98a1bff6b7269e27bd060f
Jonathan Chang <jonoleighchang@gmail.com>
2020-03-01 15:15:18 -0400

    Annoy-O-Matic complete

--- Javascript/Annoy-O-Matic.js
+++ Javascript/Annoy-O-Matic.js
@@ -1,9 +1,19 @@
+//Version 1
+
+// var ques = prompt("Are we there yet?");
+
+// while(ques !== "yes" && ques !== "yeah") {
+//     var ques = prompt("Are we there yet?");
+// }
+
+// alert("Yay, we made it!!!");
+
+//Version 2
+
 var ques = prompt("Are we there yet?");
 
-var lowerQues = ques.toLowerCase
-
-while(lowerQues != ("yes" || "yeah")) {
-    if(lowerQues === ("yes" || "yeah")) {
-        alert("Yay, we finally made it!")
-    }
+while(ques.indexOf("yes") === -1 && ques.indexOf("yeah") === -1 ) {
+     var ques = prompt("Are we there yet?");
 }
+
+alert("Yay, we made it!!!");
