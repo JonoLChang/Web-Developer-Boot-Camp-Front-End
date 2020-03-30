d5105bdcdfa9eaa9331d50b67700c30bd218040e
Jonathan Chang <jonoleighchang@gmail.com>
2020-03-30 16:28:50 -0400

    jQuery Selector Exercise Completed

---
+++ jQuery/jQuerySelectorExercise.js
@@ -0,0 +1,7 @@
+$("div").css("background", "purple");
+
+$(".highlight").width("200px"); //or .css("width", "200px");
+
+$("#third").css("border", "10px solid orange");
+
+$("div").eq(0).css("color", "pink"); // or $("div:first-of-type").css("color", "pink");
