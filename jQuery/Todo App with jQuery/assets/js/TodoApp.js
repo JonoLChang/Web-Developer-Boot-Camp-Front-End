211e75431cbf2f4d09714d088f778f80a32e6be7
Jonathan Chang <jonoleighchang@gmail.com>
2020-04-02 12:11:03 -0400

    Todo App Completed. Good shit in here

--- jQuery/Todo App with jQuery/assets/js/TodoApp.js
+++ jQuery/Todo App with jQuery/assets/js/TodoApp.js
@@ -1,4 +1,27 @@
 // Check off specific Todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+       $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+// click() and on() do the same job but on() will add listeners for all potential elements
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){ //.which is the code for each key on a keyboard. 13 represents the enter key
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
 });
